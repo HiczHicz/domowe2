4ac76a11740814cb51473f9fe2d0c2173b0b085a
Martyna Pieczka <125594476+HiczHicz@users.noreply.github.com>
2025-06-09 09:36:37 +0200

    Update MyPanel.java
    dodanie komentarzy, taki projekt oddałam

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,44 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.util.stream.*;
 public class MyPanel extends JPanel {
-    int PUNKTY = 1000000;
     double EPSILON = 1e-6;
     private Complex PIERWIASTEK1 = new Complex(1, 0);
     private Complex PIERWIASTEK2 = new Complex(-0.5, Math.sqrt(3) / 2);
     private Complex PIERWIASTEK3 = new Complex(-0.5, -Math.sqrt(3) / 2);
 
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-
-
-        java.util.stream.Stream.generate(() -> {
-            double x = Math.random() * 3.0 - 1.5;
+        Stream.generate(() -> {
+            double x = Math.random() * 3.0 - 1.5; //math.random generuje liczbę z przedziału [0,1]
             double y = Math.random() * 3.0 - 1.5;
-            return new double[]{x, y};
-        }).limit(PUNKTY).forEach(point -> {//spróbować to zrobić bez tablicy
+            return new double[]{x, y}; //zapisanie w tablicy - najprościej odzyskać
+        }).limit(100000).forEach(point -> {
             double x = point[0];
             double y = point[1];
             Complex z = new Complex(x, y);
-            Complex result = newtonComplex(z);
-            int colorIndex = findClosestRootIndex(result);
-            Color c = switch (colorIndex) {
+            Complex wynik = newtonComplex(z);
+
+            int colorIndex = findClosestRootIndex(wynik);
+            Color c = switch (colorIndex) { //switch zastępuje powtaraznie wielokrotnie if else
                 case 0 -> Color.RED;
                 case 1 -> Color.GREEN;
                 case 2 -> Color.BLUE;
                 default -> Color.BLACK;
             };
-
-            int px = (int) ((x + 1.5) / 3.0 * getWidth());
+            int px = (int) ((x + 1.5) / 3.0 * getWidth()); //int w nawiasie sprawia, że double zmienia się w int
             int py = (int) ((1.5 - y) / 3.0 * getHeight());
 
             g.setColor(c);
             g.fillRect(px, py, 1, 1);
         });
     }
-
+// obliczanie odległości pomiędzy wygenerowaną liczbą z a pierwiastkiem
     private int findClosestRootIndex(Complex z) {
         double dist1 = z.minus(PIERWIASTEK1).abs();
         double dist2 = z.minus(PIERWIASTEK2).abs();
@@ -46,9 +41,9 @@ public class MyPanel extends JPanel {
 
         if (dist1 <= dist2 && dist1 <= dist3) return 0;
         if (dist2 <= dist1 && dist2 <= dist3) return 1;
-        return 2;
+        else return 2;
     }
-
+//metoda newtona
     private Complex newtonComplex(Complex z) {
         while (true) {
             Complex z2 = z.times(z);
